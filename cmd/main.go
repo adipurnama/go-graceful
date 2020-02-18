48858ccb1ef57359475a1ce1a26f3c0c07ad0cb6
Adi Purnama <adi.purnama234@gmail.com>
2020-02-18 17:03:13 +0700

    3. Graceful shutdown impl

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -16,6 +20,8 @@ var healthStatus string
 
 func main() {
 	// port
+	waitTimeS := flag.Int("wait", 10, "OUT_OF_SERVICE wait time in seconds")
+	shutdownTimeS := flag.Int("shutdown", 10, "Shutdown timeout")
 	port := flag.Int64("port", 8081, "Insert http port for server")
 	flag.Parse()
 
@@ -26,23 +32,46 @@ func main() {
 	router.HandleFunc("/actuator/health", healthCheck)
 	router.HandleFunc("/actuator/info", appInfo)
 
-	healthStatus = "UP"
 	s := http.Server{
 		Addr:    fmt.Sprintf(":%d", *port),
 		Handler: router,
 	}
 
-	log.Printf("serving http service at port %d \n", *port)
-	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatal("error serving http request")
+	healthStatus = "UP"
+	go func() {
+		log.Printf("serving http service at port %d \n", *port)
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("error serving http request")
+		}
+	}()
+
+	quitCh := make(chan os.Signal, 1)
+	signal.Notify(quitCh, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	<-quitCh
+	log.Println("quit signal received..")
+
+	// wait for LB see us as OUT_OF_SERVICE
+	healthStatus = "OUT_OF_SERVICE"
+	time.Sleep(time.Duration(*waitTimeS) * time.Second)
+
+	// then shutdown server with timeout
+	log.Println("Shutting down server..")
+	sCtx, cancel := context.WithTimeout(context.Background(), time.Duration(*shutdownTimeS)*time.Second)
+	defer cancel()
+	if err := s.Shutdown(sCtx); err != nil && err != http.ErrServerClosed {
+		log.Fatal("found error while shutdown server")
 	}
+	log.Println("Bye.")
 }
 
 func createOrder(w http.ResponseWriter, r *http.Request) {
+	log.Println("BEGIN - Processing Order")
 	type order struct {
 		ID string `json:"id"`
 	}
 
+	time.Sleep(5 * time.Second)
+	log.Println("DONE - Processing Order")
 	resp := order{
 		ID: uuid.New().String(),
 	}
